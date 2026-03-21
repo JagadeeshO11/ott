db64c9318b6e766096ddb85b10c7f4f4a977e84b
JagadeeshO11 <jagaosuru@gmail.com>
2026-03-21 10:01:30 +0530

    chore: obfuscate TMDB API key to VITE_APP_TOKEN

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_KEY = import.meta.env.VITE_APP_TOKEN;
 const BASE_URL = "https://api.themoviedb.org/3";
 const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
 
